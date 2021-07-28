332b20d3188dfd7c248f45a9e21c7b39147fb547
Idris Fagbemi <idrisniyi94@gmail.com>
2021-07-28 01:49:31 -0500

    creating flask application

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
 
 app = Flask(__name__)
 
+# bp = Blueprint('app', __name__, url_prefix='/app')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html', title='About us')
+    # return render_template('index.html')
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html', title='Signup')
+
+@app.route('/forgotpassword', methods=['GET', 'POST'])
+def forgotpassword():
+    return render_template('forgotpassword.html', title='Forgot Password')
 
 
 if __name__ == '__main__':
---
+++ schema.py
@@ -0,0 +1,27 @@
+import sqlite3 as sql
+from sqlite3 import Error
+
+
+try:
+    conn = sql.connect("database.db")
+
+    cur = conn.cursor()
+
+    cur.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL)")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS texts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS video (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS audio (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))")
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS details (id INTEGER PRIMARY KEY AUTOINCREMENT, details TEXT, size INTEGER, upload_type TEXT, 
+                audio_id INTEGER, FOREIGN KEY(audio_id) REFERENCES audio(id),
+                texts_id INTEGER, FOREIGN KEY(texts_id) REFERENCES texts(id),
+                video_id TEXT, FOREIGN KEY(video_id) REFERENCES video(id))""")
+
+
+    cur.close()
+    
+except Error as e:
+    print(e)
