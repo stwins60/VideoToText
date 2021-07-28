07af57f49c4779b32be3658c537b5ae26aa2d06d
Idris Fagbemi <idrisniyi94@gmail.com>
2021-07-28 07:40:06 -0500

    update errors

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
+from flask import Flask, render_template, request, session, redirect, url_for, flash, Blueprint
+import sqlite3 as db
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
 # bp = Blueprint('app', __name__, url_prefix='/app')
+
+conn = db.connect('schema.db')
+cur = db.cursor()
 
 @app.route('/')
 def index():
@@ -19,6 +24,18 @@ def login():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    msg = ''
+    if request.method  == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        
+        cur.execute("SELECT * FROM users WHERE username = % ", (username,))
+        
+        if username == 'admin' and password == 'admin':
+            return redirect(url_for('index'))
+        else:
+            msg = 'Invalid username or password'
     return render_template('signup.html', title='Signup')
 
 @app.route('/forgotpassword', methods=['GET', 'POST'])
--- schema.py
+++ schema.py
@@ -16,9 +16,8 @@ try:
     cur.execute("CREATE TABLE IF NOT EXISTS audio (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))")
 
     cur.execute("""CREATE TABLE IF NOT EXISTS details (id INTEGER PRIMARY KEY AUTOINCREMENT, details TEXT, size INTEGER, upload_type TEXT, 
-                audio_id INTEGER, FOREIGN KEY(audio_id) REFERENCES audio(id),
-                texts_id INTEGER, FOREIGN KEY(texts_id) REFERENCES texts(id),
-                video_id TEXT, FOREIGN KEY(video_id) REFERENCES video(id))""")
+                audio_id INTEGER, texts_id INTEGER, video_id INTEGER, FOREIGN KEY(audio_id) REFERENCES audio(id),
+                FOREIGN KEY(texts_id) REFERENCES texts(id), FOREIGN KEY(video_id) REFERENCES video(id))""")
 
 
     cur.close()
