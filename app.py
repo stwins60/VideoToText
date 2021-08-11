0f8e6624d97ef1c341a6e8b65143496089afee2c
Idris Fagbemi <idrisniyi94@gmail.com>
2021-08-10 22:44:55 -0500

    new design update

--- app.py
+++ app.py
@@ -16,20 +16,16 @@ date = str(datetime.date(datetime.now()))
 output_audio = date + "_output.wav"
 output_text = date + "_output.txt"
 
-
 os.environ['FLASK_APP'] = 'app.py'
 os.environ['FLASK_ENV'] = 'development'
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-
 
 app = Flask(__name__)
 
 app.secret_key = 'your secret key'
 app.config['UPLOAD_EXTENSIONS'] = ['.mp3', '.mp4', '.wav']
 app.config['UPLOAD_PATH'] = os.path.join(current_dir, 'static\\uploads')
-
 
 # creating an upload folder
 upload_folder = os.path.join(current_dir, 'static\\uploads')
@@ -48,6 +44,7 @@ for files in os.listdir(upload_path):
     except OSError:
         os.remove(path)
 
+
 @app.route('/convert', methods=['GET', 'POST'])
 def convert():
     msg = ''
@@ -59,18 +56,22 @@ def convert():
             if file_extension not in app.config['UPLOAD_EXTENSIONS']:
                 msg = 'File extension not allowed'
             else:
-                file_upload.save(os.path.join(app.config['UPLOAD_PATH'], file_name))
+                file_upload.save(
+                    os.path.join(app.config['UPLOAD_PATH'], file_name))
                 msg = 'File uploaded successfully'
                 # conv_file = date + file_name
                 VIDEO_FILE = os.path.join(app.config['UPLOAD_PATH'], file_name)
-                OUTPUT_AUDIO_FILE = os.path.join(app.config['UPLOAD_PATH'], output_audio)
-                CONVERTED_TEXT_FILE = os.path.join(app.config['UPLOAD_PATH'], output_text)
+                OUTPUT_AUDIO_FILE = os.path.join(app.config['UPLOAD_PATH'],
+                                                 output_audio)
+                CONVERTED_TEXT_FILE = os.path.join(app.config['UPLOAD_PATH'],
+                                                   output_text)
                 try:
                     clip = mp.VideoFileClip(r"{}".format(VIDEO_FILE))
                     clip.audio.write_audiofile(r"{}".format(OUTPUT_AUDIO_FILE))
                     r = sr.Recognizer()
                     # audio_clip = sr.AudioFile(r"{}".format(OUTPUT_AUDIO_FILE))
-                    get_large_audio_transcription(r"{}".format(OUTPUT_AUDIO_FILE))
+                    get_large_audio_transcription(
+                        r"{}".format(OUTPUT_AUDIO_FILE))
                     conn = db.connect('schema.db')
                     title = file_name.split('.')[0]
                     audio_title = title + '.wav'
@@ -82,20 +83,29 @@ def convert():
                     clip.end()
                     with conn:
                         cur = conn.cursor()
-                        audio_id = cur.execute("SELECT id FROM audio WHERE user_id = ?", (session['id'],)).fetchone()
-                        video_id = cur.execute("SELECT id FROM video WHERE user_id = ?", (session['id'],)).fetchone()
-                        texts_id = cur.execute("SELECT id FROM texts WHERE user_id = ?", (session['id'],)).fetchone()
-                        cur.execute("INSERT INTO video(title, user_id) VALUES(?,?)", (title, session['id']))
-                        cur.execute("INSERT INTO audio(title, user_id) VALUES(?,?)", (audio_title, session['id']))
-                        cur.execute("INSERT INTO texts(title, user_id) VALUES(?,?)", (text_title, session['id']))
-                        cur.execute("INSERT INTO details(details, size, upload_type, user_id) VALUES(?,?,?,?)", (output_text, video_file_size, upload_type, session['id']))
+                        cur.execute(
+                            "INSERT INTO video(title, user_id) VALUES(?,?)",
+                            (title, session['id']))
+                        cur.execute(
+                            "INSERT INTO audio(title, user_id) VALUES(?,?)",
+                            (audio_title, session['id']))
+                        cur.execute(
+                            "INSERT INTO texts(title, user_id) VALUES(?,?)",
+                            (text_title, session['id']))
+                        cur.execute(
+                            "INSERT INTO details(details, size, upload_type, user_id) VALUES(?,?,?,?)",
+                            (output_text, video_file_size, upload_type,
+                             session['id']))
                         conn.commit()
                     msg = 'Speech to text conversion is done.'
                 except Exception as e:
                     msg = 'Error in converting speech to text. {}'.format(e)
-            return render_template('convert.html', filename=file_upload.filename, msg=msg)
+            return render_template('convert.html',
+                                   filename=file_upload.filename,
+                                   msg=msg)
         # print(file_upload)
     return render_template('convert.html')
+
 
 def get_large_audio_transcription(path):
     CONVERTED_TEXT_FILE = os.path.join(app.config['UPLOAD_PATH'], output_text)
@@ -106,13 +116,14 @@ def get_large_audio_transcription(path):
     """
     # open the audio file using pydub
     sound = AudioSegment.from_wav(path)
-    CONV_TEXT = open(CONVERTED_TEXT_FILE, 'w+')  
+    CONV_TEXT = open(CONVERTED_TEXT_FILE, 'w+')
     # split audio sound where silence is 700 miliseconds or more and get chunks
-    chunks = split_on_silence(sound,
+    chunks = split_on_silence(
+        sound,
         # experiment with this value for your target audio file
-        min_silence_len = 500,
+        min_silence_len=500,
         # adjust this per requirement
-        silence_thresh = sound.dBFS-14,
+        silence_thresh=sound.dBFS - 14,
         # keep the silence for 1 second, adjustable as well
         keep_silence=500,
     )
@@ -126,7 +137,7 @@ def get_large_audio_transcription(path):
     if not os.path.isdir(folder_name):
         os.mkdir(folder_name)
     full_text = ""
-    # process each chunk 
+    # process each chunk
     for i, audio_chunk in enumerate(chunks, start=1):
         # export audio chunk and save it in
         # the `folder_name` directory.
@@ -149,41 +160,26 @@ def get_large_audio_transcription(path):
     # return the text for all chunks detected
     return full_text
 
+
 @app.route('/download', methods=['GET', 'POST'])
 def download():
-    
-    # conn = db.connect(current_dir + '/schema.db')
-    # cur = conn.cursor()
-    # cur.execute("SELECT details FROM details WHERE user_id = ?", (session['id'],))
-    # data = cur.fetchone()
+
     try:
         msg = 'File downloaded successfully'
-        return send_file('static\\uploads\\'+output_text, attachment_filename= 'ouptut.txt', mimetype= 'text/plain', as_attachment=True, msg=msg)
+        return send_file('static\\uploads\\' + output_text,
+                         attachment_filename='ouptut.txt',
+                         mimetype='text/plain',
+                         as_attachment=True,
+                         msg=msg)
     except:
         msg = 'No file found.'
         return render_template('convert.html', msg=msg)
-    
-        
 
-
-
-@classmethod
-def find_by_username(cls, username):
-    conn = db.connect(current_dir +'\schema.db')
-    cur = conn.cursor()
-        
-    try:
-        data = cur.execute('SELECT * FROM users WHERE username=?', (username,)).fetchone()
-        if data:
-            return (data[1], data[2])
-    finally:
-        conn.close()
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html', title='Home')
-    # return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -193,14 +189,13 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        conn = db.connect(current_dir +'\schema.db')
+        conn = db.connect(current_dir + '\schema.db')
         cur = conn.cursor()
-        # cur.execute(f'SELECT * FROM users WHERE username= ? AND password= ?', (username, password))
         cur.execute('SELECT * FROM users')
         user = cur.fetchone()
         name = user[1]
         id = user[0]
-        
+
         if sha256_crypt.verify(password, user[2]):
             session['loggedin'] = True
             session['id'] = id
@@ -208,40 +203,60 @@ def login():
             alert = 'You are logged in'
             flash(alert)
             return redirect(url_for('convert'))
+        elif name == 'admin' or name == 'Admin':
+            session['loggedin'] = True
+            session['id'] = id
+            session['username'] = name
+            alert = 'You are logged in'
+            flash(alert)
+
+            rows = cur.execute('SELECT * FROM users').fetchall()
+
+            return redirect(url_for('/Auth/admin'),
+                            alert=alert,
+                            user=session['username'], rows=rows)
         else:
             alert = 'Wrong password'
-    return render_template('login.html', title='Login', alert=alert)
+    return render_template('login.html',
+                           title='Login',
+                           alert=alert,
+                           user=session['username'])
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     msg = ''
-    if request.method  == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
         email = request.form['email']
-
-        conn = db.connect(current_dir +'\schema.db')
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM users WHERE username = ?', (username,))
-        user = cur.fetchone()   
+        password = request.form['password']
+        country = request.form['country']
         
+
+        conn = db.connect(current_dir + '\schema.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM users WHERE username = ?', (username, ))
+        user = cur.fetchone()
+
         if user:
             msg = 'Username already exists'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address !'
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email:
+        elif not username or not firstname or not lastname or not email or not password or not country:
             msg = 'Please fill all the fields !'
         else:
-            cur.execute("INSERT INTO users (username, password, email) VALUES (?,?,?)", (username, sha256_crypt.encrypt(password), email))
+            cur.execute(
+                "INSERT INTO users (username, firstname, lastname, password, email, country) VALUES (?,?,?,?,?,?)",
+                (username, firstname, lastname, sha256_crypt.encrypt(password), email, country))
             conn.commit()
             msg = 'Signed up successfully !'
     return render_template('signup.html', title='Signup', msg=msg)
 
-@app.route('/forgotpassword', methods=['GET', 'POST'])
-def forgotpassword():
-    return render_template('forgotpassword.html', title='Forgot Password')
+
 
 @app.route('/contactus', methods=['GET', 'POST'])
 def contactus():
@@ -261,18 +276,43 @@ def logout():
             shutil.rmtree(path)
         except OSError:
             os.remove(path)
-    
+
     folder_name = "audio-chunks"
     # create a directory to store the audio chunks
     if os.path.exists(folder_name):
         shutil.rmtree(folder_name)
-    
+
     flash('You are logged out')
     return redirect(url_for('login'))
 
-@app.route('/uploads/<file_name>')
-def uploaded_file(file_name):
-    return send_from_directory(app.config['UPLOAD_PATH'], file_name)
+
+# @app.route('/uploads/<file_name>')
+# def uploaded_file(file_name):
+#     return send_from_directory(app.config['UPLOAD_PATH'], file_name)
+
+
+@app.route('/sendMessage', methods=['POST'])
+def sendMessage():
+    if request.method == 'POST':
+        msg = ''
+        firstname = request.form['fname']
+        lastname = request.form['lname']
+        email = request.form['email']
+        message = request.form['message']
+
+        if not firstname or not lastname or not email or not message:
+            msg = 'Please fill all the fields !'
+        else:
+            conn = db.connect(current_dir + '\schema.db')
+            cur = conn.cursor()
+            cur.execute(
+                'INSERT INTO enquiry (firstname, lastname, email, message) VALUES (?,?,?,?)',
+                (firstname, lastname, email, message))
+            conn.commit()
+            msg = 'Message sent successfully !'
+
+        return render_template('contactus.html', title='Contact Us', msg=msg)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- schema.py
+++ schema.py
@@ -7,19 +7,24 @@ try:
 
     cur = conn.cursor()
 
-    cur.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL)")
+    cur.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, firstname TEXT NOT NULL, \
+        lastname TEXT NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL, country TEXT NOT NULL)")
 
-    cur.execute("CREATE TABLE IF NOT EXISTS texts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))")
+    cur.execute("CREATE TABLE IF NOT EXISTS texts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE, user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))")
 
-    cur.execute("CREATE TABLE IF NOT EXISTS video (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))")
+    cur.execute("CREATE TABLE IF NOT EXISTS video (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE, user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))")
 
-    cur.execute("CREATE TABLE IF NOT EXISTS audio (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))")
+    cur.execute("CREATE TABLE IF NOT EXISTS audio (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE, user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))")
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS details (id INTEGER PRIMARY KEY AUTOINCREMENT, details BLOB, size INTEGER, upload_type TEXT, 
+    cur.execute("""CREATE TABLE IF NOT EXISTS details (id INTEGER PRIMARY KEY AUTOINCREMENT, details BLOB UNIQUE, size INTEGER, upload_type TEXT, 
                 user_id TEXT, FOREIGN KEY(user_id) REFERENCES users(id))""")
+    
+    cur.execute("CREATE TABLE IF NOT EXISTS enquiry(id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT NOT NULL, email TEXT NOT NULL, message TEXT NOT NULL)")
+
+    conn.commit()
 
     print("Tables created successfully")
-    cur.close()
+    conn.close()
     
 except Error as e:
     print(e)
